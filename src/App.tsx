5fa2a4c0da0dc44ae5be6ead2a67b6cd4b1b7550
torihedden <tori.hedden@gmail.com>
2025-03-12 00:46:22 +0000

    Add consts, todos

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,10 @@ import { scoreRound } from './Scoring/Scoring';
 
 import './App.css';
 import { Die } from './Die/Die';
-
-const initialDice = [
-  { id: 'A', number: 0 },
-  { id: 'B', number: 0 },
-  { id: 'C', number: 0 },
-  { id: 'D', number: 0 },
-  { id: 'E', number: 0 },
-  { id: 'F', number: 0 },
-];
+import { initialDice, WINNING_SCORE } from './constants';
 
 function App() {
-  // const [roundScore, setRoundScore] = useState(0);
+  const [roundScore, setRoundScore] = useState(0);
   const [gameScore, setGameScore] = useState(0);
 
   const [dice, setDice] = useState<Array<Die>>(initialDice);
@@ -23,7 +15,7 @@ function App() {
   const [heldDice, setHeldDice] = useState<Array<string>>([]);
   const [scoredDice, setScoredDice] = useState<Array<string>>([]);
 
-  const isInitialGameRoll =
+  const isInitialRoll =
     dice.every((d, _, arr) => d.number === arr[0].number) &&
     dice[0].number === 0;
 
@@ -74,14 +66,17 @@ function App() {
       <div className="wrapper">
         <button
           onClick={() => rollDice(getUnHeldDice())}
-          disabled={heldDice.length === 6} // TODO: add case where roll yields no scorable dice,
+          disabled={heldDice.length === 6}
+          // TODO: add case where roll yields no scorable dice, the player busts and scores no points
+          // this is more relevant in two-player games but is still a case to consider
+          // since it immediately yields 0 points for that round's score
         >
           Roll
         </button>
       </div>
 
       <div className="dice-wrapper">
-        {!isInitialGameRoll &&
+        {!isInitialRoll &&
           getUnHeldDice().map((id) => (
             <Die
               die={getDie(id)}
@@ -106,24 +101,43 @@ function App() {
         <div>
           <button
             onClick={() => {
-              setGameScore(
+              setRoundScore(
                 gameScore + scoreRound(heldDice.map((d) => getDie(d))),
               );
               addDieToScoredDice(heldDice);
               setHeldDice([]);
             }}
+            disabled={heldDice.length === 0}
           >
             Score selected dice
           </button>
         </div>
         <br />
-        <div>total game score: {gameScore}</div>
+        <hr />
+        <div>round score: {roundScore}</div>
         <br />
-        <div>scored/ineligible for further rolls dice:</div>
         <div className="dice-wrapper">
           {scoredDice.map((id) => (
             <Die die={getDie(id)} onClick={() => undefined} key={id} />
           ))}
+        </div>
+        <br />
+        <button
+          onClick={() => {
+            setGameScore(gameScore + roundScore);
+            setDice(initialDice);
+            setHeldDice([]);
+            setScoredDice([]);
+            setRoundScore(0);
+          }}
+          disabled={roundScore === 0}
+        >
+          End turn and score
+        </button>
+        <br />
+        <br />
+        <div>
+          game score: {gameScore} / {WINNING_SCORE}
         </div>
       </div>
     </>
---
+++ src/constants.tsx
@@ -0,0 +1,10 @@
+export const initialDice = [
+  { id: 'A', number: 0 },
+  { id: 'B', number: 0 },
+  { id: 'C', number: 0 },
+  { id: 'D', number: 0 },
+  { id: 'E', number: 0 },
+  { id: 'F', number: 0 },
+];
+
+export const WINNING_SCORE = 4000;
